dc7ff914ce1e0bedb553f257aa6a9177fd6ce306
Robert S Crawford <craw.rs@gmail.com>
2015-11-07 12:30:26 -0800

    Committing after implementing filter functionality. Next is authentication.

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -12,6 +12,19 @@ angular.module('shortly.links', ['shortly.services'])
     console.dir($scope.links);
   });
 
+  $scope.linkClick = function (url) {
+    console.dir(url);
+    window.location.href = url.base_url + '/api/links/' + url.code;
+  };
+
+  $scope.linksFilterFunction = function (filterInput) {
+    if(!$scope.linksFilter){
+      return true;
+    }
+    return filterInput.url.toLowerCase().indexOf($scope.linksFilter.toLowerCase()) !== -1 ||
+           filterInput.title.toLowerCase().indexOf($scope.linksFilter.toLowerCase()) !== -1;
+  };
+
   $scope.logout = function () {
     Auth.signout();
   }
